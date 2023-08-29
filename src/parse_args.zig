e83a8a70a4acbad2451482e24ab36a2683c5869c
Daniel Gross <daniel.gross.2@gmail.com>
2023-08-29 12:36:54 -0400

    Refactor arg parsing, add flag, improve errors

--- src/parse_args.zig
+++ src/parse_args.zig
@@ -9,13 +9,15 @@ const MacIpAddressPair = main.MacIpAddressPair;
 pub fn parseArgs(alloc: std.mem.Allocator) !ExecutionOptions {
     const params = comptime clap.parseParamsComptime(
         \\-h, --help Display this help and exit
-        \\-i, --interface <INTERFACE_NAME> Network interface name on which to listen and send packets
-        \\<MAPPING>... One or more MAC to IP (v4 or v6) address mappings, each one in the format <MAC address>|<IP address>
+        \\-i, --interface <IFACE> Network interface name on which to listen and send packets
+        \\-m, --mapping <MAPPING>... One or more MAC to IP (v4 or v6) address mappings, each one in the format <MAC address>,<IP address>
+        \\    --mappings <MAPPINGS> A single string containing one or more mappings in the format <MAC address>,<IP address> with mappings separated by a space
         \\
     );
     const parsers = comptime .{
-        .INTERFACE_NAME = clap.parsers.string,
+        .IFACE = clap.parsers.string,
         .MAPPING = clap.parsers.string,
+        .MAPPINGS = clap.parsers.string,
     };
     var diag = clap.Diagnostic{};
     var res = clap.parse(clap.Help, &params, parsers, .{
@@ -42,42 +44,25 @@ pub fn parseArgs(alloc: std.mem.Allocator) !ExecutionOptions {
 
     log.debug("Interface: {?s}", .{res.args.interface});
 
-    const mappings = try parseMacIpMappings(alloc, res.positionals);
+    const mappings = try parseMacIpMappings(alloc, res.args.mapping, res.args.mappings);
     execution_options.ip4_mappings = mappings[0];
     execution_options.ip6_mappings = mappings[1];
 
     return execution_options;
 }
 
-fn parseMacIpMappings(alloc: std.mem.Allocator, args: []const []const u8) ![2][]const MacIpAddressPair {
+fn parseMacIpMappings(alloc: std.mem.Allocator, mapping_args: []const []const u8, mapping_str_arg: ?[]const u8) ![2][]const MacIpAddressPair {
     var ip4_mappings = std.ArrayList(MacIpAddressPair).init(alloc);
     var ip6_mappings = std.ArrayList(MacIpAddressPair).init(alloc);
 
-    for (args) |arg| {
-        log.debug("Positional arg: {s}", .{arg});
+    for (mapping_args) |arg| {
+        try parseMacIpMapping(arg, &ip4_mappings, &ip6_mappings);
+    }
 
-        var it = std.mem.tokenizeScalar(u8, arg, '|');
-        const mac_addr_slice = it.next() orelse return error.InvalidArgument;
-        var mac_addr: [6]u8 = undefined;
-        var mac_addr_iter = std.mem.tokenizeScalar(u8, mac_addr_slice, ':');
-        var i: usize = 0;
-        while (mac_addr_iter.next()) |hexpair| {
-            _ = try std.fmt.hexToBytes(mac_addr[i .. i + 1], hexpair);
-            i += 1;
-        }
-        if (it.next()) |nextVal| {
-            if (std.net.Address.parseIp4(nextVal, 0)) |ip4| {
-                try ip4_mappings.append(MacIpAddressPair{
-                    .mac = mac_addr,
-                    .ip = ip4,
-                });
-            } else |_| {}
-            if (std.net.Address.parseIp6(nextVal, 0)) |ip6| {
-                try ip6_mappings.append(MacIpAddressPair{
-                    .mac = mac_addr,
-                    .ip = ip6,
-                });
-            } else |_| {}
+    if (mapping_str_arg != null) {
+        var mapping_str_arg_iter = std.mem.tokenizeScalar(u8, mapping_str_arg.?, ' ');
+        while (mapping_str_arg_iter.next()) |arg| {
+            try parseMacIpMapping(arg, &ip4_mappings, &ip6_mappings);
         }
     }
 
@@ -89,6 +74,60 @@ fn parseMacIpMappings(alloc: std.mem.Allocator, args: []const []const u8) ![2][]
     }
 
     return [_][]MacIpAddressPair{ try ip4_mappings.toOwnedSlice(), try ip6_mappings.toOwnedSlice() };
+}
+
+fn parseMacIpMapping(mapping_arg: []const u8, ip4_mappings: *std.ArrayListAligned(MacIpAddressPair, null), ip6_mappings: *std.ArrayListAligned(MacIpAddressPair, null)) !void {
+    log.debug("Mapping arg: {s}", .{mapping_arg});
+    parseMacIpMappingContent(mapping_arg, ip4_mappings, ip6_mappings) catch {
+        log.err("Failed to parse mapping argument: {s}. Mapping should be a MAC address, followed by a comma, followed by an IP address (no spaces)", .{mapping_arg});
+        return error.InvalidMappingArgument;
+    };
+}
+
+fn parseMacIpMappingContent(mapping_arg: []const u8, ip4_mappings: *std.ArrayListAligned(MacIpAddressPair, null), ip6_mappings: *std.ArrayListAligned(MacIpAddressPair, null)) !void {
+    var mapping_arg_iter = std.mem.tokenizeScalar(u8, mapping_arg, ',');
+    const mac_addr_slice = mapping_arg_iter.next() orelse unreachable; // mapping_arg is expected not to be empty
+
+    if (mac_addr_slice.len != 17) {
+        log.err("First part of mapping does not look like a MAC address: {s}. Use format AA:BB:CC:DD:EE:FF.", .{mac_addr_slice});
+        return error.InvalidMacAddr;
+    }
+
+    var mac_addr: [6]u8 = undefined;
+    var mac_addr_iter = std.mem.tokenizeScalar(u8, mac_addr_slice, ':');
+    var i: usize = 0;
+    while (mac_addr_iter.next()) |hexpair| {
+        _ = std.fmt.hexToBytes(mac_addr[i .. i + 1], hexpair) catch {
+            log.err("Failed to parse MAC address: {s}", .{mac_addr_slice});
+            return error.InvalidMacAddr;
+        };
+        i += 1;
+    }
+
+    const ip_addr_slice = mapping_arg_iter.next() orelse {
+        return error.IncompleteMappingArgument;
+    };
+
+    if (mapping_arg_iter.next() != null) {
+        log.err("Mapping should only have one comma.", .{});
+        return error.InvalidMappingArgument;
+    }
+
+    if (std.net.Address.parseIp4(ip_addr_slice, 0)) |ip4| {
+        return ip4_mappings.append(MacIpAddressPair{
+            .mac = mac_addr,
+            .ip = ip4,
+        });
+    } else |_| {}
+    if (std.net.Address.parseIp6(ip_addr_slice, 0)) |ip6| {
+        return ip6_mappings.append(MacIpAddressPair{
+            .mac = mac_addr,
+            .ip = ip6,
+        });
+    } else |_| {}
+
+    log.err("Failed to parse IP address as IPv4 or IPv6: .{s}", .{ip_addr_slice});
+    return error.InvalidIpAddr;
 }
 
 test "expect correctly parsed mappings with 1 IPv4 mapping arg" {
